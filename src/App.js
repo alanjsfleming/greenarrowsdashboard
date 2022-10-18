c9bdaa1ec5a70fe9d26a69370470067bd280f5d4
alanjsfleming <alanjsfleming@gmail.com>
2022-10-18 19:52:42 +0100

    removed unused files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Dashboard from './Dashboard';
+import { useState,useEffect } from 'react';
+const dweetURL = 'https://dweet.io/get/latest/dweet/for/Albyn1'
+
+function estimateGear(Spd,RPM) {
+  let wheelRPM=Spd/(59.44*60/63360);
+  let gearRatio=RPM/wheelRPM;
+  let numTeethMotor=58/gearRatio;
+  let gearNumber=Math.round(21.5-numTeethMotor);
+  return(gearNumber);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Dashboard />
+    <input type="number"/>
+    <button>Submit</button>
+    </>
   );
 }
 
 export default App;
+
+/*
+<div className="App">
+      <header className="App-header">
+        
+        <h1>
+          Green Arrows Dashboard
+        </h1>
+        <section class="primary-data">
+          <div>
+            <h3>BatteryPercent</h3>
+          </div>
+        </section>
+        
+      </header>
+    </div>
+    */
