fe6547379b84f0226c2a3024b5d09a718887065e
alanjsfleming <alanjsfleming@gmail.com>
2022-10-19 15:27:41 +0100

    Added saving settings and started styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React, { useState, useRef, useEffect } from 'react';
 import Dashboard from './Dashboard';
+import "./App.css"
 
 /*import { useState,useEffect } from 'react';*/
 const ga1dweetname='https://dweet.io/get/latest/dweet/for/Albyn1'
@@ -12,21 +13,49 @@ const ga1dweetname='https://dweet.io/get/latest/dweet/for/Albyn1'
 
 function App() {
   const [telemetry, newTelemetry] = useState([]);
-  const carSettingsRef = useRef();
-  
+  const [settings, newSettings] = useState([])
+  const GearSettingsRef = useRef();
+  const AmpHourSettingsRef = useRef()
+  const LOCAL_STORAGE_SETTINGS_KEY='dashboardApp.settings'
 
-  function handleSaveCarSettings(e) {
-    const bigGear = carSettingsRef.current.value
-    console.log(carSettingsRef.current.value)
-    return bigGear
+  /* Load saved settings  */
+
+  useEffect(() => {
+    const storedSettings = JSON.parse(localStorage.getItem(LOCAL_STORAGE_SETTINGS_KEY));
+    if (storedSettings) {newSettings(storedSettings)};
+  },[]) 
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_SETTINGS_KEY, JSON.stringify(settings))
+  },[settings])
+
+  function handleSaveGearSettings(e) {
+    const bigGear = GearSettingsRef.current.value
+    newSettings(prevSettings => ({
+      ...prevSettings,
+      bigGear:bigGear
+      }))
   };
 
+
+
+  function handleSaveAmpHourSettings(e){
+    const ampHours = AmpHourSettingsRef.current.value
+    newSettings(prevSettings => ({
+      ...prevSettings,
+      ampHours:ampHours
+    }))
+  }
+
+
+
   function handleUpdateTelemetry(e) {
+    console.log(localStorage)
+    console.log(settings,JSON.parse(localStorage.getItem(LOCAL_STORAGE_SETTINGS_KEY)))
     fetch(ga1dweetname)
     .then((response)=>response.json())
     .then((data)=> { 
-      console.log(data.with[0].content)
-      newTelemetry([data.with[0].content])
+     newTelemetry([data.with[0].content])
     });
   }
 
@@ -38,14 +67,17 @@ function App() {
 
   return (
     <>
-    <Dashboard telemetry={telemetry}/>
-    <label>Number of teeth on big gear</label>
-    <input id="bigGear" ref={carSettingsRef} type="number"/>
-    <button onClick={handleSaveCarSettings}>Save</button>
-    <label>Total Amp hours of Batteries</label>
-    <input id="ampHours" ref={carSettingsRef} type="number"/>
-    <button onClick={handleSaveCarSettings}>Save</button>
-    <button onClick={handleUpdateTelemetry}>Update Data</button>
+    <Dashboard telemetry={telemetry} settings={settings}/>
+    <label>Number of teeth on big gear (currently {settings.bigGear})</label>
+    <input class="form" ref={GearSettingsRef} type="number" defaultValue={settings.bigGear}/>
+    <button class="btn btn-primary" onClick={handleSaveGearSettings}>Save</button>
+    <br></br>
+    <label>Total Amp hours of batteries (currently {settings.ampHours} )</label>
+    <input ref={AmpHourSettingsRef} type="number" defaultValue={settings.ampHours}/>
+    <button class="btn btn-primary" onClick={handleSaveAmpHourSettings}>Save</button>
+    <br></br>
+    <button class="btn btn-primary" onClick={handleUpdateTelemetry}>Update Data</button>
+  
     </>
   );
 }
--- src/Telemetry.js
+++ src/Telemetry.js
@@ -1,33 +1,41 @@
 import React from 'react'
 
 
-function estimateGear(Spd,RPM) {
+function estimateGear(Spd,RPM,bG) {
     let wheelRPM=Spd/(59.44*60/63360);
     let gearRatio=RPM/wheelRPM;
-    let numTeethMotor=58/gearRatio;
+    let numTeethMotor=bG/gearRatio;
     let gearNumber=Math.round(21.5-numTeethMotor);
     return(gearNumber);
   }
 
-  
+function calculateBatteryPercent(ah,ahtotal) {
+  const percent = ((ahtotal-ah)/ahtotal*100).toFixed(1)
+  return percent
+} 
 
-export default function Telemetry({telemetry}) {
+export default function Telemetry({telemetry,settings}) {
   return (
     <>
-    <div class="gauge gear">Estimated Gear : {estimateGear((telemetry.Spd*2.237),telemetry.RPM)}</div>
-    <div class="gauge">Vt : {telemetry.Vt}</div>
-    <div>V1 : {telemetry.V1}</div>
-    <div>V2 : {telemetry.Vt-telemetry.V1}</div>
-    <div>A : {telemetry.A}</div>
-    <div>Motor RPM : {telemetry.RPM}</div>
-    <div>Speed : {telemetry.Spd*2.237}</div>
-    <div>Throttle : {telemetry.Thrtl}</div>
-    <div>Amp Hours : {telemetry.AH}</div>
-    <div>Lap : {telemetry.lap}</div>
-    <div>Temperature 1 : {telemetry.Tmp1}</div>
-    <div>Temperature 2 : {telemetry.Tmp2}</div>
-    <div>Break : {telemetry.Brk}</div>
-    <div>Distance : {telemetry.Distance}</div>
+    <div class="grid-container">
+    <div class="telem gauge gear">Estimated Gear : {estimateGear((telemetry.Spd*2.237),telemetry.RPM,settings.bigGear)}</div>
+    <div class="telem battery">Battery : {calculateBatteryPercent(telemetry.AH,settings.ampHours)}%
+    
+    </div>
+    <div class="telem gauge">Vt : {telemetry.Vt}V</div>
+    <div class="telem gauge">V1 : {telemetry.V1}V</div>
+    <div class="telem gauge">V2 : {telemetry.Vt-telemetry.V1}V</div>
+    <div class="telem gauge">A : {telemetry.A}A</div>
+    <div class="telem gauge">Motor RPM : {telemetry.RPM}</div>
+    <div class="telem gauge">Speed : {(telemetry.Spd*2.237).toFixed(1)} mph</div>
+    <div class="telem gauge">Throttle : {telemetry.Thrtl}%</div>
+    <div class="telem">Amp Hours used: {telemetry.AH}Ah</div>
+    <div class="telem gauge">Lap : {telemetry.lap}</div>
+    <div class="telem thermometer">Temperature 1 : {telemetry.Tmp1}&deg;C</div>
+    <div class="telem thermometer">Temperature 2 : {telemetry.Tmp2}&deg;C</div>
+    <div class="telem">Break : {telemetry.Brk}></div>
+    <div class="telem">Distance : {telemetry.Distance}</div>
+    </div>
     </>
   )
 }
