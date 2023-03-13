cc3cb710e2b20eed7188d40bd8ff2701d21a0649
alanjsfleming <alanjsfleming@gmail.com>
2023-03-13 11:55:18 +0000

    Error message if dashboard cant fetch data

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -12,6 +12,8 @@ export default function HomePage() {
 
   const cardweetname='https://dweet.io/get/latest/dweet/for/Albyn1'
   const [telemetry, newTelemetry] = useState([]);
+  const [settings, newSettings] = useState()
+  const [errorFetching, setErrorFetching] = useState(0)
 
   const { currentUser } = useAuth()
   
@@ -20,10 +22,11 @@ export default function HomePage() {
     fetch(cardweetname)
     .then((response)=>response.json())
     .then((data)=> { 
+      setErrorFetching(0)
      newTelemetry([data.with[0].content])
     })
     .catch(e =>{
-      
+      setErrorFetching(errorFetching+1)
     });
   }
 
@@ -36,6 +39,7 @@ export default function HomePage() {
     <>
     <MenuBar />
     <h2 class="home-page-card--title py-3">{currentUser.displayName} Dashboard</h2>
+    {errorFetching>8 && <div class="alert alert-warning text-center mx-2">Unable to fetch data from car.<br></br> Check that Dweet has data.</div>}
     <div class="d-flex flex-column">
       <CarSummary name={'GA1'} telemetry={telemetry}/>
       <br></br>
