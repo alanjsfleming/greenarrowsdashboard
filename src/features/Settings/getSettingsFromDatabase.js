70e311dc5a136f918af086378c91d119404c7836
Alan Fleming <114114685+alanjsfleming@users.noreply.github.com>
2023-12-26 22:48:16 +0000

    function to get car settings from database

--- src/features/Settings/getSettingsFromDatabase.js
+++ src/features/Settings/getSettingsFromDatabase.js
@@ -1,6 +1,6 @@
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, getDocs } from "firebase/firestore";
 
-export default async function getSettingsFromDatabase(uid) {
+async function getUserSettingsFromDatabase(uid) {
     const docRef = doc(db, "users", uid);
     try {
         const docSnap = await getDoc(docRef);
@@ -11,5 +11,25 @@ export default async function getSettingsFromDatabase(uid) {
         }
     } catch (e) {
         throw new Error('Failed to get document: ' + e.message);
+    }
+}
+
+async function getCarSettingsFromDatabase(uid) {
+    const carQuery = query(collection(db,"cars"),where("owner","==",uid),orderBy("car_number"));
+    const carQuerySnapshot = await getDocs(carQuery);
+    let carSettings = [];
+    carQuerySnapshot.forEach((doc) => {
+        let carData = doc.data();
+        carData.id = doc.id;
+        carSettings.push(doc.data());
+    });
+}
+
+export default async function getSettingsFromDatabase(uid) {
+    try {
+        const userSettings = await getUserSettingsFromDatabase(uid);
+        return userSettings.settings;
+    } catch (e) {
+        throw new Error('Failed to get settings: ' + e.message);
     }
 }
--- src/features/Settings/mergeSettingsObjects.js
+++ src/features/Settings/mergeSettingsObjects.js
@@ -9,8 +9,17 @@ export default function mergeSettings(obj1,obj2) {
     let mergedObject = mergeObjects(obj1,obj2);
 
     // Merge the cars arrays
-    for (let i = 0; i < cars1.length; i++) {
-        mergedObject.cars[i] = mergeObjects(cars1[i],cars2[i])
+    for (let i = 0; i < Math.max(cars1.length,cars2.length); i++) {
+        if (i < cars1.length && i < cars2.length) {
+            // Both cars1 and cars2 have a car at this index
+            mergedObject.cars[i] = mergeObjects(cars1[i],cars2[i])
+        } else if (i < cars2.length) {
+            // Only cars2 has a car at this index, this is a new car!
+            mergedObject.cars[i] = cars2[i]
+        } else {
+            // Only cars1 has a car at this index, this car has been deleted
+            mergedObject.cars[i] = cars1[i]
+        }
     }
     
     return mergedObject;
